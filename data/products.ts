4b3c3d81506c1b81242db0a2104c56f5eb4e32df
islom8414 <ismatchanov08@gmail.com>
2026-06-26 21:35:16 +0900

    Initial commit: puzzle catalog with 50 products

---
+++ data/products.ts
@@ -0,0 +1,413 @@
+export interface Product {
+  id: string;
+  name: string;
+  category: string;
+  price: number;
+  image: string;
+  pieces: number;
+}
+
+export const products: Product[] = [
+  {
+    id: "1",
+    name: "Симпатичный щенок",
+    category: "Животные",
+    price: 890,
+    image: "/photo_2026-06-26_21-05-59.jpg",
+    pieces: 500
+  },
+  {
+    id: "2",
+    name: "Милый котенок",
+    category: "Животные",
+    price: 950,
+    image: "/photo_2026-06-26_21-06-19.jpg",
+    pieces: 500
+  },
+  {
+    id: "3",
+    name: "Золотистый ретривер",
+    category: "Животные",
+    price: 1200,
+    image: "/photo_2026-06-26_21-06-39.jpg",
+    pieces: 1000
+  },
+  {
+    id: "4",
+    name: "Игривая собачка",
+    category: "Животные",
+    price: 850,
+    image: "/photo_2026-06-26_21-06-44.jpg",
+    pieces: 300
+  },
+  {
+    id: "5",
+    name: "Лабрадор на природе",
+    category: "Животные",
+    price: 1100,
+    image: "/photo_2026-06-26_21-06-48.jpg",
+    pieces: 750
+  },
+  {
+    id: "6",
+    name: "Щенок в траве",
+    category: "Животные",
+    price: 990,
+    image: "/photo_2026-06-26_21-06-53.jpg",
+    pieces: 500
+  },
+  {
+    id: "7",
+    name: "Корги в саду",
+    category: "Животные",
+    price: 1350,
+    image: "/photo_2026-06-26_21-07-01.jpg",
+    pieces: 1000
+  },
+  {
+    id: "8",
+    name: "Два щенка",
+    category: "Животные",
+    price: 1450,
+    image: "/photo_2026-06-26_21-07-07.jpg",
+    pieces: 1500
+  },
+  {
+    id: "9",
+    name: "Маленький пёсик",
+    category: "Животные",
+    price: 790,
+    image: "/photo_2026-06-26_21-07-14.jpg",
+    pieces: 300
+  },
+  {
+    id: "10",
+    name: "Щенок с игрушкой",
+    category: "Животные",
+    price: 1150,
+    image: "/photo_2026-06-26_21-07-18.jpg",
+    pieces: 750
+  },
+  {
+    id: "11",
+    name: "Счастливый щенок",
+    category: "Животные",
+    price: 1050,
+    image: "/photo_2026-06-26_21-07-22.jpg",
+    pieces: 500
+  },
+  {
+    id: "12",
+    name: "Щенок на лужайке",
+    category: "Животные",
+    price: 980,
+    image: "/photo_2026-06-26_21-10-31.jpg",
+    pieces: 500
+  },
+  {
+    id: "13",
+    name: "Маленький йорк",
+    category: "Животные",
+    price: 890,
+    image: "/photo_2026-06-26_21-10-37.jpg",
+    pieces: 300
+  },
+  {
+    id: "14",
+    name: "Щенок в движении",
+    category: "Животные",
+    price: 750,
+    image: "/photo_2026-06-26_21-10-42.jpg",
+    pieces: 200
+  },
+  {
+    id: "15",
+    name: "Игривый щенок",
+    category: "Животные",
+    price: 820,
+    image: "/photo_2026-06-26_21-10-46.jpg",
+    pieces: 300
+  },
+  {
+    id: "16",
+    name: "Щенок с цветами",
+    category: "Животные",
+    price: 950,
+    image: "/photo_2026-06-26_21-10-50.jpg",
+    pieces: 500
+  },
+  {
+    id: "17",
+    name: "Милый пёсик",
+    category: "Животные",
+    price: 880,
+    image: "/photo_2026-06-26_21-10-53.jpg",
+    pieces: 300
+  },
+  {
+    id: "18",
+    name: "Щенок в поле",
+    category: "Животные",
+    price: 920,
+    image: "/photo_2026-06-26_21-10-57.jpg",
+    pieces: 400
+  },
+  {
+    id: "19",
+    name: "Щенок с листом",
+    category: "Животные",
+    price: 850,
+    image: "/photo_2026-06-26_21-11-01.jpg",
+    pieces: 300
+  },
+  {
+    id: "20",
+    name: "Щенок на прогулке",
+    category: "Животные",
+    price: 990,
+    image: "/photo_2026-06-26_21-11-05.jpg",
+    pieces: 500
+  },
+  {
+    id: "21",
+    name: "Щенок с мячом",
+    category: "Животные",
+    price: 1050,
+    image: "/photo_2026-06-26_21-11-09.jpg",
+    pieces: 500
+  },
+  {
+    id: "22",
+    name: "Щенок в лесу",
+    category: "Животные",
+    price: 1100,
+    image: "/photo_2026-06-26_21-11-13.jpg",
+    pieces: 750
+  },
+  {
+    id: "23",
+    name: "Щенок с бабочкой",
+    category: "Животные",
+    price: 1250,
+    image: "/photo_2026-06-26_21-11-16.jpg",
+    pieces: 1000
+  },
+  {
+    id: "24",
+    name: "Щенок с цветами",
+    category: "Животные",
+    price: 1650,
+    image: "/photo_2026-06-26_21-11-24.jpg",
+    pieces: 1500
+  },
+  {
+    id: "25",
+    name: "Щенок на закате",
+    category: "Животные",
+    price: 1550,
+    image: "/photo_2026-06-26_21-11-28.jpg",
+    pieces: 1500
+  },
+  {
+    id: "26",
+    name: "Щенок в саду",
+    category: "Животные",
+    price: 1450,
+    image: "/photo_2026-06-26_21-11-31.jpg",
+    pieces: 1000
+  },
+  {
+    id: "27",
+    name: "Щенок с розами",
+    category: "Животные",
+    price: 1750,
+    image: "/photo_2026-06-26_21-11-35.jpg",
+    pieces: 2000
+  },
+  {
+    id: "28",
+    name: "Щенок с ромашками",
+    category: "Животные",
+    price: 1680,
+    image: "/photo_2026-06-26_21-11-39.jpg",
+    pieces: 2000
+  },
+  {
+    id: "29",
+    name: "Щенок в цветах",
+    category: "Животные",
+    price: 1590,
+    image: "/photo_2026-06-26_21-11-43.jpg",
+    pieces: 1500
+  },
+  {
+    id: "30",
+    name: "Щенок с подсолнухами",
+    category: "Животные",
+    price: 1850,
+    image: "/photo_2026-06-26_21-11-51.jpg",
+    pieces: 2000
+  },
+  {
+    id: "31",
+    name: "Щенок в поле цветов",
+    category: "Животные",
+    price: 1950,
+    image: "/photo_2026-06-26_21-11-54.jpg",
+    pieces: 2500
+  },
+  {
+    id: "32",
+    name: "Щенок с пионами",
+    category: "Животные",
+    price: 2100,
+    image: "/photo_2026-06-26_21-11-58.jpg",
+    pieces: 3000
+  },
+  {
+    id: "33",
+    name: "Щенок с тюльпанами",
+    category: "Животные",
+    price: 1450,
+    image: "/photo_2026-06-26_21-12-02.jpg",
+    pieces: 1000
+  },
+  {
+    id: "34",
+    name: "Щенок с лилиями",
+    category: "Животные",
+    price: 1750,
+    image: "/photo_2026-06-26_21-12-05.jpg",
+    pieces: 2000
+  },
+  {
+    id: "35",
+    name: "Щенок в саду роз",
+    category: "Животные",
+    price: 1890,
+    image: "/photo_2026-06-26_21-12-09.jpg",
+    pieces: 2500
+  },
+  {
+    id: "36",
+    name: "Щенок с фиалками",
+    category: "Животные",
+    price: 1920,
+    image: "/photo_2026-06-26_21-12-16.jpg",
+    pieces: 2500
+  },
+  {
+    id: "37",
+    name: "Щенок с нарциссами",
+    category: "Животные",
+    price: 1850,
+    image: "/photo_2026-06-26_21-12-20.jpg",
+    pieces: 2000
+  },
+  {
+    id: "38",
+    name: "Щенок с астрами",
+    category: "Животные",
+    price: 1550,
+    image: "/photo_2026-06-26_21-12-24.jpg",
+    pieces: 1500
+  },
+  {
+    id: "39",
+    name: "Щенок с хризантемами",
+    category: "Животные",
+    price: 1450,
+    image: "/photo_2026-06-26_21-12-27.jpg",
+    pieces: 1000
+  },
+  {
+    id: "40",
+    name: "Щенок с георгинами",
+    category: "Животные",
+    price: 1390,
+    image: "/photo_2026-06-26_21-12-31.jpg",
+    pieces: 1000
+  },
+  {
+    id: "41",
+    name: "Щенок с пеларгонией",
+    category: "Животные",
+    price: 1950,
+    image: "/photo_2026-06-26_21-12-34.jpg",
+    pieces: 2500
+  },
+  {
+    id: "42",
+    name: "Щенок с петуниями",
+    category: "Животные",
+    price: 1790,
+    image: "/photo_2026-06-26_21-12-38.jpg",
+    pieces: 2000
+  },
+  {
+    id: "43",
+    name: "Щенок с календулой",
+    category: "Животные",
+    price: 1920,
+    image: "/photo_2026-06-26_21-12-41.jpg",
+    pieces: 2500
+  },
+  {
+    id: "44",
+    name: "Щенок в лугу",
+    category: "Животные",
+    price: 890,
+    image: "/photo_2026-06-26_21-12-55.jpg",
+    pieces: 300
+  },
+  {
+    id: "45",
+    name: "Щенок на поляне",
+    category: "Животные",
+    price: 950,
+    image: "/photo_2026-06-26_21-13-02.jpg",
+    pieces: 500
+  },
+  {
+    id: "46",
+    name: "Щенок с травой",
+    category: "Животные",
+    price: 980,
+    image: "/photo_2026-06-26_21-13-05.jpg",
+    pieces: 500
+  },
+  {
+    id: "47",
+    name: "Щенок в зелени",
+    category: "Животные",
+    price: 870,
+    image: "/photo_2026-06-26_21-13-12.jpg",
+    pieces: 300
+  },
+  {
+    id: "48",
+    name: "Щенок на лугу",
+    category: "Животные",
+    price: 790,
+    image: "/photo_2026-06-26_21-13-18.jpg",
+    pieces: 200
+  },
+  {
+    id: "49",
+    name: "Щенок в траве",
+    category: "Животные",
+    price: 820,
+    image: "/photo_2026-06-26_21-13-21.jpg",
+    pieces: 300
+  },
+  {
+    id: "50",
+    name: "Щенок на природе",
+    category: "Животные",
+    price: 890,
+    image: "/photo_2026-06-26_21-13-25.jpg",
+    pieces: 300
+  }
+];
+
+export const categories = ["Животные", "Природа", "Архитектура", "Искусство"];
